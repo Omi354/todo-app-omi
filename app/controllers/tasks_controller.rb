b7999b98501a59d972923f20ab5ea8cbd82369ef
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-08-30 16:42:37 +0900

    タスクの編集機能を実装しました

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,6 +22,21 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def edit
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def update
+    @task = current_user.tasks.find(params[:id])
+    if @task.update(task_params.merge(board_id: params[:board_id]))
+      flash[:notice] = "タスクの編集に成功しました"
+      redirect_to board_tasks_path(board_id: params[:board_id])
+    else
+      flash.now[:alert] = "タスクの編集に失敗しました"
+      render :edit
+    end
+  end
+
   private
   def task_params
     params.require(:task).permit(:name, :desc, :deadline)
