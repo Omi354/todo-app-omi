5cb5d31a667b5083d49a9c6b6b7ec8eaf5a53c73
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-08-31 01:25:21 +0900

    コメントモデルを作成し、新規コメントを作成する機能と、taskのshowページ内に表示する機能を実装しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :boards
   has_many :tasks
+  has_many :comments
+
   validates :user_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
