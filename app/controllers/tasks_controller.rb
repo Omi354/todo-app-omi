a0d248e4582d944c7f8c57def66a759502b67ae6
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-08-30 15:20:07 +0900

    ・boardのフォームで:descが反映されないエラーを修正しました ・taksのnewページを作成しました ・taskに新規作成機能を追加しました

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,25 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.where(board_id: params[:board_id])
   end
+
+  def new
+    @task = current_user.tasks.build(board_id: params[:board_id])
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params.merge(board_id: params[:board_id]))
+    if @task.save
+      flash[:notice] = "タスクの作成に成功しました"
+      redirect_to board_tasks_path(board_id: params[:board_id])
+    else
+      flash.now[:alert] = "タスクの作成に失敗しました"
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :desc, :deadline)
+  end
+
 end
