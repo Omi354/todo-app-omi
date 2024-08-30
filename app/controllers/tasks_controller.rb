e980d372f38093ab49263218f37c321a5b34c883
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-08-30 21:56:26 +0900

    taskにファイルアップロードの機能を実装しました

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,7 +46,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:name, :desc, :deadline)
+    params.require(:task).permit(:name, :desc, :deadline, :image)
   end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :board
+  has_one_attached :image
+
+  validates :name, presence: true
+
 end
